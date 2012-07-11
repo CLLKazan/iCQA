e07da2747fd84ddc90d2a0f4a244182f6aa4bdc1
nzhiltsov <nikita.zhiltsov@gmail.com>
2012-07-11 16:53:56 +0400

    Add computing topic probabilities and sampling from their distribution #14

--- analytic-module/R/answerer-recommendation/R/TrainUQA.R
+++ analytic-module/R/answerer-recommendation/R/TrainUQA.R
@@ -26,10 +26,24 @@ TrainUQA <- function(db.channel, db.name, user.profiles) {
                                                            user.profiles,
                                                            topic.number)},
                                   .progress="text")
+  topics <- unique(topic.word.assignments$topic_id)
   # Computes overall frequencies
-  word.topic.frequencies <- as.data.frame(with(topic.word.assignments,table(word_id,topic_id)))
-  category.topic.frequencies <- as.data.frame(with(topic.word.assignments,table(category_id,topic_id)))
-  user.topic.frequencies <- as.data.frame(with(topic.word.assignments,table(user_id,topic_id)))
+  word.topic.frequencies <- ddply(topic.word.assignments,
+                                .(topic.word.assignments$word_id,
+                                  topic.word.assignments$topic_id),
+                                  nrow)
+  names(word.topic.frequencies) <- c("word_id", "topic_id", "Freq")
+  category.topic.frequencies <- ddply(topic.word.assignments,
+                                      .(topic.word.assignments$category_id,
+                                        topic.word.assignments$topic_id),
+                                      nrow)
+  names(category.topic.frequencies) <- c("category_id", "topic_id", "Freq")
+  user.topic.frequencies <- ddply(topic.word.assignments,
+                                  .(topic.word.assignments$user_id,
+                                    topic.word.assignments$topic_id),
+                                  nrow)
+  names(user.topic.frequencies) <- c("user_id", "topic_id", "Freq")
+  category.number <- length(unique(category.topic.frequencies$category_id))
   # Processes assignments
   by(topic.word.assignments,  1:nrow(topic.word.assignments),
         function(row) {ProcessAssignment(row,
@@ -37,7 +51,8 @@ TrainUQA <- function(db.channel, db.name, user.profiles) {
                                       category.topic.frequencies,
                                       user.topic.frequencies,
                                       topic.word.assignments,
-                                      topic.number)})  
+                                      topics,
+                                      category.number)}, simplify=T)  
 
   return (topic.word.assignments)
 }
@@ -74,39 +89,90 @@ ProcessAssignment <- function(assignment,
                               category.topic.frequencies,
                               user.topic.frequencies,
                               topic.word.assignments,
-                              topic.number) {
+                              topics,
+                              category.number) {
   # Decreases the related frequencies
   word.topic.freq <- with(word.topic.frequencies,
-                  word.topic.frequencies[which(word_id==assignment$word_id && 
+                  word.topic.frequencies[which(word_id==assignment$word_id & 
          topic_id==assignment$topic_id),])
-  if (nrow(word.topic.freq) > 0 && word.topic.freq$Freq > 0) {
+  if (nrow(word.topic.freq) > 0 & word.topic.freq$Freq > 0) {
         word.topic.freq$Freq <- word.topic.freq$Freq - 1
   }
   category.topic.freq <- with(category.topic.frequencies,
                               category.topic.frequencies[
-                              which(category_id==assignment$category_id && 
+                              which(category_id==assignment$category_id & 
                               topic_id==assignment$topic_id),])
-  if (nrow(category.topic.freq) && category.topic.freq$Freq > 0) {
+  if (nrow(category.topic.freq) & category.topic.freq$Freq > 0) {
     category.topic.freq$Freq <- category.topic.freq$Freq - 1
   }
   user.topic.freq <- with(user.topic.frequencies,
                           user.topic.frequencies[
-                            which(user_id==assignment$user_id && 
+                            which(user_id==assignment$user_id & 
                               topic_id==assignment$topic_id),])
-  if (nrow(user.topic.freq) && user.topic.freq$Freq > 0) {
+  if (nrow(user.topic.freq) & user.topic.freq$Freq > 0) {
     user.topic.freq$Freq <- user.topic.freq$Freq - 1
   }
-  t <- with(topic.word.assignments, topic.word.assignments[
-    which(user_id==assignment$user_id && topic_id==assignment$topic_id),])
-  l.uz.ui <- nrow(t)
-  t <- with(topic.word.assignments, topic.word.assignments[
-    which(user_id==assignment$user_id && word_id==assignment$word_id && 
-      topic_id==assignment$topic_id),])
-  n.z.ui.w.ui <- nrow(t)
-  
-#   (l.uz.ui + 50/topic.number - 1)*(n.z.ui.w.ui + .05 - 1)/()
+  # Computes topic probabilities
+  topic.number <- length(topics)
+  topic.probabilities <- llply(topics,
+                               function(topic) {
+                                 ComputeTopicProbability(topic.word.assignments,
+                                                         word.topic.frequencies,
+                                                         category.topic.frequencies,
+                                                         assignment,
+                                                         topic,
+                                                         category.number,
+                                                         topic.number)})
+#   print(topic.number)
+  topic.probabilities <- unlist(topic.probabilities)
+#   print(topic.probabilities)
+  sampled.topic.id <- sample(topics, 1, prob=topic.probabilities)
+#   print(sample.topic.id)
+#   topic.probabilities <- unlist(topic.probabilities)
+#   print(topic.probabilities)
+#   sampled.topic <- sample(topics, 1, prob=topic.probabilities)
+#   print(sampled.topic)
+#   stop("error")
 }
-# ComputeTopicProbability <- function() {
-#   (user.topic.frequencies[assignment$user_id, assignment$topic_id] +
-#     50/topic.number - 1)*()
-# }
+
+ComputeTopicProbability <- function(topic.word.assignments,
+                                      word.topic.frequencies,
+                                      category.topic.frequencies,
+                                      assignment,
+                                      topic.id,
+                                      category.number,
+                                      topic.number) {
+  
+  t <- with(topic.word.assignments, topic.word.assignments[
+    which(user_id==assignment$user_id & topic_id==topic.id),])
+  l.uz.ui <- nrow(t)
+  
+  word.freqs <- 
+    with(word.topic.frequencies, 
+         word.topic.frequencies[which(topic_id==topic.id),]$Freq)
+  n.z.ui.w.ui <- 
+    with(word.topic.frequencies, 
+         word.topic.frequencies[which(word_id==assignment$word_id &
+           topic_id==topic.id),]$Freq)
+  if (length(n.z.ui.w.ui)==0) {
+    n.z.ui.w.ui <- 0
+  }
+  beta.params <- rep(.05, length(word.freqs))
+  n.z.ui.v <- word.freqs + beta.params
+  
+  m.z.ui.c.ui <- with(category.topic.frequencies,
+                      category.topic.frequencies[which(category_id==assignment$category_id & 
+                        topic_id==topic.id),]$Freq)
+  if (length(m.z.ui.c.ui)==0) {
+    m.z.ui.c.ui <- 0
+  }
+  category.freqs <- 
+    with(category.topic.frequencies, 
+         category.topic.frequencies[which(topic_id==topic.id),]$Freq)
+  gamma.params <- rep(50/category.number, length(category.freqs))
+  m.z.ui.c <- category.freqs + gamma.params
+  topic.probability <- 
+    (l.uz.ui + 50/topic.number - 1)*(n.z.ui.w.ui + .05 - 1)*
+    (m.z.ui.c.ui + 50/category.number - 1)/((sum(n.z.ui.v) - 1)*(sum(m.z.ui.c) - 1))
+  return (topic.probability)
+}
