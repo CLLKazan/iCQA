73fe84bba67e746ce726676f7edfb2fd56645096
nzhiltsov <nikita.zhiltsov@gmail.com>
2012-07-18 22:46:32 +0400

    Elaborate BM25 scoring #14

--- analytic-module/R/answerer-recommendation/R/ComputeBM25F.R
+++ analytic-module/R/answerer-recommendation/R/ComputeBM25F.R
@@ -5,7 +5,10 @@ ComputeBM25F <- function(dtm,
                          question.offset,
                          candidate.title.offset,
                          candidate.body.offset,
-                         candidate.tag.offset) {
+                         candidate.tag.offset,
+                         average.title.length,
+                         average.body.length,
+                         average.tag.length) {
   # Computes a BM25F score between two given questions (in IR sense, 
   # the first question plays the role of a 'query' and 
   # the second one does the same for a 'document') represented as vectors
@@ -22,12 +25,22 @@ ComputeBM25F <- function(dtm,
   #   Frame, containing the question pairs with scores
   require(tm)
   question.id <- as.integer(rownames(question))
-  candidate.question.ids <- candidate.title.offset:dtm$nrow
-  candidate.question.number <- candidate.dtm$nrow
+  candidate.question.id <- as.integer(rownames(candidate.question))
+  # Finds the question's terms
   question.terms <- 
     tm::findFreqTerms(dtm[question.offset+question.id,],1)
   question.term.ids <- 
     laply(question.terms, function(term) {which(tm::Terms(dtm)[]==term)})
+  # Finds the candidate's fields' lengths
+  candidate.title.length <- sum(dtm[candidate.title.offset+candidate.question.id, ]$v)
+  candidate.body.length <- sum(dtm[candidate.body.offset+candidate.question.id, ]$v)
+  candidate.tag.length <- sum(dtm[candidate.tag.offset+candidate.question.id, ]$v)
+  # Setting up the field weights and free parameters
+  tag.weight <- 1
+  body.weight <- 2
+  title.weight <- 3
+  b <- 0.75
+  k1 <- 2
   # Computes IDFs
   question.term.ids.in.candidate.dtm <- 
     laply(question.terms, function(term) {which(tm::Terms(candidate.dtm)[]==term)})
@@ -35,4 +48,17 @@ ComputeBM25F <- function(dtm,
     document.frequency <- length(candidate.dtm[,id]$i)
     log( (candidate.dtm$nrow - document.frequency + 0.5) / (document.frequency + 0.5) ) 
   })
+  term.frequency.weights <- laply(question.term.ids, function(term) {
+    title.term.freq <- dtm[candidate.title.offset + candidate.question.id, term]$v
+    body.term.freq <- dtm[candidate.body.offset + candidate.question.id, term]$v
+    tag.term.freq <- dtm[candidate.tag.offset + candidate.question.id, term]$v
+    term.frequency.weight <- 
+      title.weight*title.term.freq/(1 - b + b*candidate.title.length/average.title.length) + 
+      body.weight*body.term.freq/(1 - b + b*candidate.body.length/average.body.length) +  
+      tag.weight*tag.term.freq/(1 - b + b*candidate.tag.length/average.tag.length)
+    term.frequency.weight
+  })
+  bm25.score <- 
+    (idfs) %*% (term.frequency.weights/(k1 + term.frequency.weights))
+  return (bm25.score[[1]])
 }
